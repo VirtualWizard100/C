4444a0521e7ab864d41ecf1d185f09f19187d934
VirtualWizard100 <139145827+VirtualWizard100@users.noreply.github.com>
2025-07-17 15:56:51 -0400

    Update web_server.c

--- web_server.c
+++ web_server.c
@@ -1,4 +1,6 @@
-                                                                   #include <stdio.h>
+                                                                   
+
+#include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
